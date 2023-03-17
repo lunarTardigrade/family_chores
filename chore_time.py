ea82ab36943299d5076619247936834def16659d
lunarTardigrade <87677609+lunarTardigrade@users.noreply.github.com>
2023-03-16 20:19:10 -0700

    Add files via upload

---
+++ chore_time.py
@@ -0,0 +1,264 @@
+import json
+import datetime
+import operator
+
+chore_states = ["unassigned", "assigned", "completed"]
+
+class FamilyMember:
+    def __init__(self, name, is_admin=False, scores=None):
+        self.name = name
+        self.is_admin = is_admin
+        self.scores = scores if scores is not None else []
+
+    def add_points(self, chore):
+        score_date = chore.date_completed
+        score_points = chore.point_value
+        score = Score(score_points, score_date)
+        self.scores.append(score)
+
+        with open("family.json", 'r') as f:
+            data = json.load(f)
+
+        for member_data in data:
+            if member_data['name'] == self.name:
+                member_data['scores'].append(score.__dict__)
+
+        with open("family.json", 'w') as f:
+            json.dump(data, f, indent=4,cls=choreEncoder)
+
+    def remove_points(self, chore):
+        for score in self.scores:
+            if score.date == chore.date_completed and score.points == chore.point_value:
+                self.scores.remove(score)
+                with open("family.json", 'r+') as f:
+                    data = json.load(f)
+                    member_data = next(member for member in data if member['name'] == self.name)
+                    member_data['scores'] = [s.__dict__ for s in self.scores]
+                    f.seek(0)
+                    json.dump(data, f, indent=4, cls=choreEncoder)
+                    f.truncate()
+                return True
+        return False
+
+    def get_points(self, start_date=None, end_date=None):
+        total_points = 0
+        for score in self.scores:
+            score_date = score.date
+            if start_date is None or score_date >= start_date:
+                if end_date is None or score_date <= end_date:
+                    total_points += score.points
+        return total_points
+    
+
+class Score:
+    def __init__(self, points, date=None):
+        self.points = points
+        self.date = date or datetime.now().date()
+
+class Chore:
+    def __init__(self, chore_id, name, frequency, room, blacklist=[],assigned_to = None,date_assigned=None,point_value=1):
+        self.chore_id = chore_id
+        self.name = name
+        self.frequency = frequency
+        self.room = room
+        self.blacklist = blacklist
+        self.assigned_to = assigned_to
+        self.date_assigned = date_assigned
+        self.completed = False
+        self.date_completed = None
+        self.point_value = point_value
+
+
+    def add_to_blacklist(self, family_member):
+        self.blacklist.append(family_member)
+
+    def remove_from_blacklist(self, family_member):
+        if family_member in self.blacklist:
+            self.blacklist.remove(family_member)
+
+    def is_assignable_to(self, family_member):
+        if family_member in self.blacklist:
+            return False
+        return True
+        
+    def mark_completed(self):
+        self.completed = True
+        self.date_completed = datetime.date.today()
+        family_member = get_member_by_name(self.assigned_to)
+        family_member.add_points(self)
+
+    def mark_incomplete(self):
+        self.completed = False
+        family_member = get_member_by_name(self.assigned_to)
+        family_member.remove_points(self)
+        self.date_completed = None
+
+
+    def assign(self, family_member_name, assignment_date):
+        self.assigned_to = family_member_name
+        self.date_assigned = assignment_date
+        self.completed = False
+        self.date_completed = None
+
+
+class ChoreRepository:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def load_chores(self):
+        try:
+            with open(self.file_path, 'r') as f:
+                data = json.load(f)
+            chores = {}
+            for chore_id, chore_data in data.items():
+                date_assigned = None
+                if chore_data['date_assigned'] is not None:
+                    date_assigned = datetime.datetime.strptime(chore_data['date_assigned'], '%Y-%m-%d').date()
+                date_completed = None
+                if chore_data['date_completed'] is not None:
+                    date_completed = datetime.datetime.strptime(chore_data['date_completed'], '%Y-%m-%d').date()
+                chore = Chore(chore_id=chore_id,
+                            name=chore_data['name'],
+                            frequency=int(chore_data['frequency']),
+                            room=chore_data['room'],
+                            blacklist=chore_data['blacklist'],
+                            assigned_to=chore_data['assigned_to'],
+                            date_assigned=date_assigned,
+                            point_value=chore_data['point_value'])
+                chore.completed = chore_data['completed']
+                chore.date_completed = date_completed
+                chores[chore_id] = chore
+            return chores
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            return {}
+
+    def save_chores_to_file(self, chores:dict):
+        chores_to_save = {}
+        for chore in chores.values():
+            if isinstance(chore, Chore):
+                chores_to_save[chore.chore_id] = chore.__dict__
+        with open(self.file_path, 'w') as file:
+            json.dump(chores_to_save, file, cls=choreEncoder, indent=4)
+
+    def update_chore(self, chore):
+        chores = self.load_chores()
+        chores[chore.chore_id] = chore
+        self.save_chores_to_file(chores)
+
+
+def new_chore(chores,name,room,frequency,family_members,point_value=1):
+    chore_id = name.lower().replace(" ", "") + "_" + room.lower().replace("room", "").replace(" ", "")
+    new_chore = Chore(chore_id, name, frequency, room)
+    new_chore.assigned_to = None
+    new_chore.date_assigned = None
+    new_chore.completed = False
+    new_chore.date_completed = None
+    new_chore.point_value=point_value
+    chores[new_chore.chore_id]=new_chore
+    chores = generate_assignments(chores,family_members)
+    ChoreRepository("chores.json").save_chores_to_file(chores)
+    return chores
+
+
+def load_family_members(file_path):
+    family_members = []
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        for member in data:
+            scores = []
+            for score_data in member['scores']:
+                score_date = datetime.datetime.strptime(score_data['date'], '%Y-%m-%d').date()
+                score_points = score_data['points']
+                score = Score(score_points, score_date)
+                scores.append(score)
+            family_member = FamilyMember(member['name'], member['is_admin'], scores)
+            family_members.append(family_member)
+    return family_members
+
+def get_member_by_name(name):
+    family_members = load_family_members("family.json")
+    for member in family_members:
+        if member.name == name:
+            return member
+    return None
+
+
+def get_leaderboard(start_date=None, end_date=None):
+    family_members = load_family_members("family.json")
+    leaderboard = []
+    for member in family_members:
+        points = member.get_points(start_date=start_date, end_date=end_date)
+        leaderboard.append((member.name, points))
+    leaderboard = sorted(leaderboard, key=operator.itemgetter(1), reverse=True)
+    #leaderboard_with_positions = []
+    #for i, (name, points) in enumerate(leaderboard[:4], start=1):
+    #    position = f"{i}{'th' if 11 <= i <= 13 else {1: 'st', 2: 'nd', 3: 'rd'}.get(i % 10, 'th')}"
+    #    leaderboard_with_positions.append((position, name, points))
+    return leaderboard
+
+
+def generate_assignments(chores, family_members):
+    current_date = datetime.date.today()
+
+    for chore_id, chore in chores.items():
+        start_date = chore.date_completed or None
+        frequency = chore.frequency
+
+        next_assignment_date = get_next_assignment_date(start_date, frequency)
+
+        if (next_assignment_date - current_date).days <= 7:
+            if (chore.completed and (current_date - chore.date_completed).days >= 1) or chore.assigned_to == None:
+                eligible_family_members = [m for m in family_members if chore.is_assignable_to(m.name)]
+
+                chore_assignments = {}
+                for member in eligible_family_members:
+                    member_assignments = [c for c in chores.values() if c.assigned_to == member.name and c.date_assigned is not None and c.date_assigned <= next_assignment_date and not c.completed]
+                    chore_assignments[member] = len(member_assignments)
+
+                if chore_assignments:
+                    assigned_to = min(chore_assignments, key=chore_assignments.get)
+                    chore.assign(assigned_to.name, next_assignment_date)
+#        save_chores_to_file(chores, "chores.json")
+    return chores
+                            
+
+class choreEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime) or isinstance(obj, datetime.date):
+            return obj.isoformat()
+        elif isinstance(obj, list):
+            return [self.default(item) for item in obj]
+        return super().default(obj)
+
+
+def generate_assignment_dates(start_date, frequency, num_days):
+    assignment_dates = []
+    current_date = start_date
+    for i in range(num_days):
+        if i % frequency == 0:
+            assignment_dates.append(current_date)
+        current_date += datetime.timedelta(days=1)
+    return assignment_dates
+
+
+def get_next_assignment_date(start_date, frequency):
+    if start_date is None:
+        next_date = datetime.date.today()
+    else:
+        start_date = start_date
+        interval = datetime.timedelta(days=frequency)
+        next_date = start_date + interval
+        if next_date < datetime.date.today():
+            next_date = datetime.date.today()
+    return next_date
+
+
+def get_todays_assignments(chores):
+    current_date = datetime.date.today()
+    assignments = {}
+    for chore_id, chore in chores.items():
+        if chore.completed and (current_date - chore.date_completed).days <= 1:
+            assignments[chore_id] = chore
+        elif not chore.completed and chore.date_assigned is not None and chore.date_assigned <= current_date:
+            assignments[chore_id] = chore
+    return assignments
---
+++ ui.py
@@ -0,0 +1,316 @@
+import chore_time
+import tkinter as tk
+import tkinter.ttk as ttk
+import datetime
+import base64
+
+class RootScreen():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("YAY ! CHORE TIME ! YAY")
+
+        # Create a Combobox for the time range selection
+        self.time_range_var = tk.StringVar(value='All Time')
+        time_range_combobox = ttk.Combobox(self.root, textvariable=self.time_range_var, state="readonly")
+        time_range_combobox['values'] = ('Today', 'This Week', 'This Month', 'This Year', 'All Time')
+        time_range_combobox.grid(row=0, column=0, padx=5, pady=5, sticky='w')
+        startDate, endDate = get_date_range(self.time_range_var.get())
+        self.leader_board = chore_time.get_leaderboard(startDate,endDate)
+        time_range_combobox.bind("<<ComboboxSelected>>", self.populate_leaderboard)
+
+        # Add #1 label
+        leader_label = tk.Label(self.root, text='#1', font=('Helvetica', 20, 'bold'))
+        leader_label.grid(row=1, column=0, columnspan=2, pady=5, padx=(50, 0), sticky='nsew')
+
+        # Add photo and name labels
+        # leader_photo = tk.Label(self.root, text='PHOTO', font=('Helvetica', 14))
+        # leader_photo.grid(row=2, column=0, pady=5, sticky='w')
+        self.leader_name = tk.Label(self.root, text=self.leader_board[0][0], font=('Helvetica', 14))
+        self.leader_name.grid(row=2, column=0, pady=5, sticky='nsew')
+        self.leader_score = tk.Label(self.root, text=self.leader_board[0][1], font=('Helvetica', 14))
+        self.leader_score.grid(row=2, column=1, pady=5, sticky='nsew')
+
+        # Add table with 3 columns
+        table_columns = ('2nd', '3rd', '4th')
+        self.table = ttk.Treeview(self.root, columns=table_columns, show='headings', height=2)
+        self.table.grid(row=3, column=0, columnspan=2, padx=5, pady=5, sticky='nsew')
+
+        # Add column headers
+        for col in table_columns:
+            self.table.heading(col, text=col, anchor='center')
+
+        self.populate_leaderboard()
+
+        # self.logo = tk.Label(self.root, text="Logo Here")
+        # self.logo.pack()
+        self.root.after(3000, self.show_assignments_screen)
+        self.parent = None  # define parent attribute
+        self.root.mainloop()
+    
+    def populate_leaderboard(self, event=None):
+        # Clear existing table 
+        startDate, endDate = get_date_range(self.time_range_var.get())
+        self.leader_board = chore_time.get_leaderboard(startDate,endDate)
+        self.table.delete(*self.table.get_children())
+
+        # Update first place winner's information
+        self.leader_name.config(text=self.leader_board[0][0])
+        self.leader_score.config(text=self.leader_board[0][1])
+
+        # Repopulate table with updated data
+        row1=[]
+        row2=[]
+        for winner, score in self.leader_board[-3:]:
+            row1.append(winner)
+            row2.append(score)
+        self.table.insert("", "end", values=row1)
+        self.table.insert("", "end", values=row2)
+
+    def show_assignments_screen(self):
+        self.root.withdraw()  # hide the root screen
+        self.parent = self.root  # set parent attribute
+        AssignmentsScreen(self.root)
+        self.parent.deiconify()
+
+
+class AssignmentsScreen():
+    def __init__(self, parent):
+        chores = chore_time.ChoreRepository("chores.json").load_chores()
+        family_members = chore_time.load_family_members("family.json")
+        chores = chore_time.generate_assignments(chores,family_members)
+        chores = chore_time.get_todays_assignments(chores)
+        window = tk.Toplevel()
+        self.parent = parent
+        self.chores = chores
+        self.family_members = family_members
+        self.frame = tk.Frame(window)
+        self.frame.pack(fill="both", expand=True)
+
+        self.table = ttk.Treeview(self.frame, columns=("id","chore", "room", "assigned_to", "date_assigned", "completed"), show="headings")
+        self.table.heading("id", text="", anchor="w")
+        self.table.column("id", width=0, stretch=tk.NO)
+        self.table.heading("chore", text = "chore")
+        self.table.heading("room", text="Room")
+        self.table.heading("assigned_to", text="Assigned To")
+        self.table.heading("date_assigned", text="Date Assigned")
+        self.table.heading("completed", text="Completed")
+        self.table.bind("<Button-1>", self.mark_completed)
+        self.table.tag_configure("completed",foreground="black", background="green")
+
+        self.table.pack(fill="both", expand=True)
+
+         # create command ribbon frame
+        command_frame = tk.Frame(self.frame, bd=1, relief=tk.RAISED)
+        command_frame.pack(side=tk.BOTTOM, fill=tk.X)
+
+        # create selector box with family member names
+        self.selected_family_member = tk.StringVar()
+        self.family_member_names = [''] + [member.name for member in self.family_members]
+        selector_label = tk.Label(command_frame, text="I want to see: ")
+        selector_label.pack(side=tk.LEFT, padx=10)
+        
+        self.selector = ttk.Combobox(command_frame, values=self.family_member_names, textvariable=self.selected_family_member)
+        self.selector.bind("<<ComboboxSelected>>", self.refresh_table)
+        self.selector.pack(side=tk.LEFT, padx=10)
+        self.selector.current(0)
+
+        # create 'New Chore' button
+        new_chore_button = tk.Button(command_frame, text="New Chore", command=self.open_new_chore_screen)
+        new_chore_button.pack(side=tk.RIGHT, padx=10) 
+        self.refresh_table()
+
+    
+    def mark_completed(self,event):
+        # Get selected chore
+        selection = self.table.selection()
+        if not selection:
+            return
+        chore_id = self.table.item(selection[0], "values")[0]
+        chore = self.chores[chore_id]
+
+        # Open MarkCompletedScreen
+        MarkCompletedScreen(self, chore)
+
+
+    def refresh_table(self, event=None):
+        # Clear existing table data
+        self.table.delete(*self.table.get_children())
+
+        # Repopulate table with updated data
+        for id, chore in self.chores.items():
+            if self.selected_family_member.get() == '' or chore.assigned_to == self.selected_family_member.get():
+                completed = "Yes" if chore.completed else "No"
+                row_values = (id,chore.name, chore.room, chore.assigned_to, chore.date_assigned.strftime("%m/%d/%Y"), completed)
+                if chore.completed:
+                    self.table.insert("", "end", values=row_values,tags=("completed",))
+                else:
+                    self.table.insert("", "end", values=row_values)
+
+        #self.table.item(self.table.get_children()[-1], values=row_values, tags=("completed",), background="green")
+
+
+    def open_new_chore_screen(self):
+        NewChoreScreen(self)
+
+
+
+
+class NewChoreScreen:
+    def __init__(self, parent):
+        self.window = tk.Toplevel(parent.parent)
+        self.parent = parent
+        self.frame = tk.Frame(self.window)
+        self.frame.pack(fill="both", expand=True)
+
+        # Create labels and input boxes for name, room, and frequency
+        tk.Label(self.frame, text="Name").grid(row=0)
+        self.name_entry = tk.Entry(self.frame)
+        self.name_entry.grid(row=0, column=1)
+
+        tk.Label(self.frame, text="Room").grid(row=1)
+        self.room_entry = tk.Entry(self.frame)
+        self.room_entry.grid(row=1, column=1)
+
+        tk.Label(self.frame, text="Frequency").grid(row=2)
+        self.frequency_entry = tk.Entry(self.frame)
+        self.frequency_entry.grid(row=2, column=1)
+
+        # Create submit button
+        self.submit_button = tk.Button(self.frame, text="Submit", command=self.submit)
+        self.submit_button.grid(row=3, column=0, columnspan=2)
+
+    def submit(self):
+        # Get values from input boxes
+        name = self.name_entry.get()
+        room = self.room_entry.get()
+        frequency = self.frequency_entry.get()
+
+        self.parent.chores = chore_time.new_chore(self.parent.chores,name,room,frequency,self.parent.family_members)
+
+        # Clear the input boxes
+        self.name_entry.delete(0, tk.END)
+        self.room_entry.delete(0, tk.END)
+        self.frequency_entry.delete(0, tk.END)
+        
+        self.parent.refresh_table()
+
+
+class MarkCompletedScreen:
+    def __init__(self, parent, chore):
+        self.window = tk.Toplevel(parent.parent)
+        self.parent = parent
+        self.chore = chore
+        self.frame = tk.Frame(self.window)
+        self.frame.pack(fill="both", expand=True)
+        self.family_members = parent.family_member_names
+
+        title_text = f"{chore.name}, {chore.room}"
+        tk.Label(self.frame, text=title_text).grid(row=0, column=0, columnspan=2, pady=10)
+        point_value_label = tk.Label(self.frame, text=f"Point Value: {chore.point_value}")
+        point_value_label.grid(row=1, column=0, columnspan=2, pady=5)
+        tk.Label(self.frame, text="Eligible:").grid(row=3, column=0, padx=5, pady=5, sticky="w")
+
+        # Create a dictionary to store the eligible checkboxes
+        self.eligible_checkboxes = {}
+
+        # Create a checkbox/label for each family member
+        for i, family_member in enumerate(parent.family_members):
+            member_name = family_member.name
+            self.eligible_checkboxes[member_name] = tk.BooleanVar(value=chore.is_assignable_to(member_name))
+            tk.Checkbutton(self.frame, text=member_name, variable=self.eligible_checkboxes[member_name], command=lambda member=member_name: self.checkbox_updated(member)).grid(row=3+i//2, column=i%2+1, padx=5, pady=5, sticky="w")
+
+        tk.Label(self.frame, text="Assigned To:").grid(row=len(self.family_members)+4, column=0, padx=5, pady=5, sticky="w")
+
+   # Create a Combobox for the "assigned to" field
+        self.assigned_to_var = tk.StringVar(value=chore.assigned_to)
+        self.assigned_to_combobox = ttk.Combobox(self.frame, textvariable=self.assigned_to_var, state="readonly")
+        self.update_assigned_to_options()
+        self.assigned_to_combobox.grid(row=len(self.family_members)+4, column=1, padx=5, pady=5, sticky="w")
+        self.assigned_to_combobox.bind("<<ComboboxSelected>>", self.update_assigned_to)
+
+        tk.Label(self.frame, text="Date Assigned:").grid(row=len(self.family_members)+5, column=0, padx=5, pady=5, sticky="w")
+        tk.Label(self.frame, text=chore.date_assigned).grid(row=len(self.family_members)+5, column=1, padx=5, pady=5, sticky="w")
+
+        if chore.completed:
+            completed_button_text = "Chore Incomplete"
+            completed_button_command = self.incomplete_chore
+            self.assigned_to_combobox.config(state="disabled")
+        else:
+            completed_button_text = "Mark as Completed"
+            completed_button_command = self.complete_chore
+
+        self.completed_button = tk.Button(self.frame, text=completed_button_text, command=completed_button_command)
+        if self.chore.assigned_to == "":
+            self.completed_button.config(state='disabled')  # disable the button
+        else:
+            self.completed_button.config(state='normal')  # enable the button
+        self.completed_button.grid(row=len(self.family_members)+6, column=0, columnspan=2, padx=5, pady=10)
+                              
+    def complete_chore(self):
+        self.chore.mark_completed()
+        self.parent.refresh_table()
+        chore_time.ChoreRepository("chores.json").update_chore(self.chore)
+        self.frame.destroy()
+        self.window.destroy()
+
+    def incomplete_chore(self):
+        self.chore.mark_incomplete()
+        self.parent.refresh_table()
+        chore_time.ChoreRepository("chores.json").update_chore(self.chore)
+        self.frame.destroy()
+        self.window.destroy()
+
+    def update_assigned_to(self, event):
+        self.chore.assigned_to = self.assigned_to_var.get()
+        self.parent.refresh_table()
+        chore_time.ChoreRepository("chores.json").update_chore(self.chore)
+        if self.chore.assigned_to == "":
+            self.completed_button.config(state='disabled')  # disable the button
+        else:
+            self.completed_button.config(state='normal')  # enable the button
+    
+    def checkbox_updated(self, family_member):
+        self.update_blacklist(family_member)
+        self.update_assigned_to_options()
+
+    def update_blacklist(self, family_member):
+        if family_member in self.chore.blacklist:
+            self.chore.remove_from_blacklist(family_member)
+        else:
+            self.chore.add_to_blacklist(family_member)
+        chore_time.ChoreRepository("chores.json").update_chore(self.chore)
+
+    def update_assigned_to_options(self):
+        blacklist = self.chore.blacklist
+        # Create a list of family members not on the blacklist
+        assigned_to_options = [member for member in self.family_members if member not in blacklist]
+        self.assigned_to_combobox['values'] = assigned_to_options
+        if self.chore.assigned_to in blacklist:
+            self.assigned_to_combobox.current(0)  # set to the first option
+        else:
+            self.assigned_to_combobox.current(0 if not self.chore.assigned_to else assigned_to_options.index(self.chore.assigned_to))
+
+
+def get_date_range(time_range):
+    today = datetime.date.today()
+    if time_range == 'Today':
+        start_date = today
+        end_date = today
+    elif time_range == 'This Week':
+        start_date = today - datetime.timedelta(days=today.weekday())
+        end_date = start_date + datetime.timedelta(days=6)
+    elif time_range == 'This Month':
+        start_date = datetime.date(today.year, today.month, 1)
+        end_date = datetime.date(today.year, today.month + 1, 1) - datetime.timedelta(days=1)
+    elif time_range == 'This Year':
+        start_date = datetime.date(today.year, 1, 1)
+        end_date = datetime.date(today.year, 12, 31)
+    else: # All Time
+        start_date = None
+        end_date = None
+    
+    return start_date, end_date
+
+
+if __name__ == '__main__':
+    root_screen = RootScreen()
